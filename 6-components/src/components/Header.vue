66ca80979ab3b68c76b80c9d0539115d1d1c4943
Pierre <pierre@pierres-macbook-pro.home>
2019-03-17 18:24:19 +0100

    CLI + components

---
+++ 6-components/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <header>
+        <h1>Server Status</h1>
+      </header>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<!-- Scoped : makes style applied to this only one component -->
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
---
+++ 6-components/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <Header></Header>
+    <hr>
+    <List></List>
+    <hr>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from './Footer.vue';
+import Header from './Header.vue';
+import List from './List.vue';
+
+export default {
+  components: {
+    Footer,
+    Header,
+    List
+  }
+}
+</script>
+
+<!-- Scoped : makes style applied to this only one component -->
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>
