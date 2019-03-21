a164e4fb271446a6d357b847aeb028fa2ba95b42
plaitse <riviera_paradise@hotmail.fr>
2019-03-21 17:25:30 +0100

    WIP: Components communication

---
+++ 7-components-communication/src/components/UserDetail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ name }}</p>
+    <p>User Name: {{ switchName() }}</p>
+    <!-- First option: castint event -->
+    <button @click="resetName">Reset name</button>
+    <!-- Second option -->
+    <button @click="resetFn">Reset name</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true, // Either this
+        default: 'Tod' // Or that
+      },
+      resetFn: Function
+    },
+    methods: {
+      switchName() {
+        return this.name.split('').reverse().join('');
+      },
+      resetName() {
+        this.name = 'Che';
+        this.$emit('nameWasReset', this.name); // Emits casting event to inform parent component
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .component {
+    background-color: lightcoral;
+  }
+</style>
