41a6e3d34355d4995742a5f08d80ee91e3d52bb4
plaitse <riviera_paradise@hotmail.fr>
2019-03-22 19:02:04 +0100

    Forms

---
+++ 10-forms-2/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <form v-if="!submitted">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <app-full-name v-model="fullName"></app-full-name>
+          <div class="form-group">
+            <label>Mail</label>
+            <input type="email" class="form-control" v-model="email">
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" v-model="password">
+          </div>
+          <div class="form-group">
+            <label>
+              <input type="radio" value="Yes" v-model="storeData"> Yes
+            </label>
+            <label>
+              <input type="radio" value="No" v-model="storeData"> No
+            </label>
+          </div>
+          <button
+            type="submit"
+            @click.prevent="submitted = true"
+            class="btn btn-primary">Submit the Form</button>
+        </div>
+      </div>
+    </form>
+    <hr>
+    <div class="row" v-if="submitted">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{ fullName }}</p>
+            <p>Mail: {{ email }}</p>
+            <p>Password: {{ password }}</p>
+            <p>Store in Database?: {{ storeData }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import FullName from './components/Fullname.vue';
+
+  export default {
+    data() {
+      return {
+        fullName: 'Malcolm X',
+        email: '',
+        password: '',
+        storeData: 'Yes',
+        submitted: false
+      }
+    },
+    components: {
+      appFullName: FullName
+    }
+  }
+</script>
+
+<style>
+</style>
