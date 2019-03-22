402bbe0e507fb61567b6da49c810041e1f829b3e
plaitse <riviera_paradise@hotmail.fr>
2019-03-22 16:49:43 +0100

    2nd mini project

---
+++ 12-mini-project/src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <form action="txt">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label for="txt">Quote</label>
+        <textarea name="txt" id="txt" rows="3" v-model="quote"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <button class="btn btn-primary" @click.prevent="createNew">Add quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        quote: ''
+      };
+    },
+    methods: {
+      createNew() {
+        this.$emit('quoteAdded', this.quote);
+        this.quote = '';
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
