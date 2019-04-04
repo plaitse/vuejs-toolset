a70c2c92f2e53737bd9bfb05bc6d19f12b447780
Pierre <pierre@Pierres-MBP.lan>
2019-04-04 17:11:55 +0200

    CSS pre-processor and ENV variables

---
+++ 19-scss/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <header>
+        <h1>Server Status</h1>
+      </header>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<!-- Scoped : makes style applied to this only one component -->
+<style scoped lang="scss">
+  div {
+    border: 1px solid blue;
+  }
+
+  div {
+    h1 {
+      display: none;
+    }
+  }
+</style>
---
+++ 20-env-variables/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <Header></Header>
+    <hr>
+    <p>{{ url }}</p>
+    <hr>
+    <List></List>
+    <hr>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from './Footer.vue';
+import Header from './Header.vue';
+import List from './List.vue';
+
+export default {
+  data() {
+    return {
+      url: process.env.VUE_APP_URL
+    }
+  },
+  components: {
+    Footer,
+    Header,
+    List
+  }
+}
+</script>
+
+<!-- Scoped : makes style applied to this only one component -->
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>
